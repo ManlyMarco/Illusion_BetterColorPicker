167b5a3d635379aa63a368f5b780b7e342c6c86f
ManlyMarco <dummyaccx@gmail.com>
2018-12-20 14:57:16 +0100

    Initial commit

---
+++ BetterColorPicker/BetterColorPicker.cs
@@ -0,0 +1,104 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+using BepInEx;
+using ChaCustom;
+using Harmony;
+using Illusion.Component.UI.ColorPicker;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BetterColorPicker
+{
+    [BepInDependency("koikoi.happy.nu.color_adjuster")]
+    [BepInProcess("Koikatu")]
+    [BepInPlugin(GUID, "Better Color Picker", Version)]
+    public class BetterColorPicker : BaseUnityPlugin
+    {
+        public const string GUID = "marco.better_color_picker";
+        public const string Version = "1.0";
+
+        private const string BtnText = "Pick color from desktop";
+        private const string BtnTextActive = "Press any button to accept";
+
+        private static PickerSliderInput _pickerSliderInput;
+        private static TextMeshProUGUI _textMeshPro;
+
+        private static bool capturing;
+        public static bool Capturing
+        {
+            get => capturing;
+            set
+            {
+                capturing = value;
+                if (_textMeshPro != null)
+                    _textMeshPro.text = capturing ? BtnTextActive : BtnText;
+            }
+        }
+
+        [DisplayName("Adjust color to saturation filter")]
+        [Description(
+            "When using default saturation filter the game colors are different than actual colors. " +
+            "Use this setting to adjust the color you capture to make it look correct under the saturation filter. " +
+            "If you do not use the saturation filter, disable this option to get the true color.")]
+        public ConfigWrapper<bool> ColorAdjust { get; private set; }
+
+        [HarmonyPatch(typeof(CvsColor), "Start")]
+        [HarmonyPostfix]
+        public static void AddPickerButton(CvsColor __instance)
+        {
+            var originalBtn = GameObject.Find("Button/textDef").transform.parent;
+            var newBtn = Instantiate(originalBtn, __instance.transform.Find("menuSlider"), true);
+            newBtn.name = "CursorPickBtn";
+
+            var rt = newBtn.GetComponent<RectTransform>();
+            rt.offsetMin = new Vector2(-369, 10);
+            rt.offsetMax = new Vector2(-20, -175);
+
+            _textMeshPro = newBtn.GetComponentInChildren<TextMeshProUGUI>();
+            _textMeshPro.text = BtnText;
+
+            var b = newBtn.GetComponent<Button>();
+            b.onClick.RemoveAllListeners();
+            b.onClick.AddListener(() => Capturing = !Capturing);
+
+            _pickerSliderInput = (PickerSliderInput) __instance.GetType().GetField("cmpPickerSliderI", BindingFlags.Instance | BindingFlags.NonPublic)?.GetValue(__instance);
+        }
+
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            Capturing = false;
+        }
+
+        private void Start()
+        {
+            ColorAdjust = new ConfigWrapper<bool>(nameof(ColorAdjust), this, true);
+
+            HarmonyInstance.Create(GUID).PatchAll(typeof(BetterColorPicker));
+        }
+
+        private void Update()
+        {
+            if (Capturing)
+            {
+                UpdateColorToPointer();
+                if (Input.anyKeyDown)
+                    Capturing = false;
+            }
+        }
+
+        private void UpdateColorToPointer()
+        {
+            if (_pickerSliderInput != null)
+            {
+                var color = MouseColour.Get();
+                color = ColorAdjust.Value ? ColorAdjuster.ColorAdjuster.Lookup(color) : color;
+                _pickerSliderInput.color = color;
+            }
+            else
+            {
+                Capturing = false;
+            }
+        }
+    }
+}
---
+++ BetterColorPicker/Properties/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("KK_BetterColorPicker")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("ManlyMarco")]
+[assembly: AssemblyProduct("KK_BetterColorPicker")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("65F1F50A-8FD4-45DE-896B-55622770200C")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion(BetterColorPicker.BetterColorPicker.Version)]
