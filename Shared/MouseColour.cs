c00a8438a712a64e14c6265268e92f755e82ff0c
IDontHaveIdea <88360628+IDontHaveIdea@users.noreply.github.com>
2021-09-11 07:26:43 -0400

    Port to KKS of KK_BetterColorPicker (#2)
    * KKS port of KK_BetterColorPicker
    
    * Typo fix version to 2.0.2 to resolve conflicts if installed in Koikatsu on different directories
    
    * Added: KKAPI version in dependency
           Processname for Studio
           Processname for Steam version
    
    * Fix: Wrong reference KKSAPI instead of KKAPI
    
    * Change: Move prefix constant to Info
            Deleted ConfigurationManagerAttributes.cs
    
    * Code cleanup

---
+++ Shared/MouseColour.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace BetterColorPicker
+{
+    public static class MouseColour
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr GetWindowDC(IntPtr window);
+        [DllImport("gdi32.dll", SetLastError = true)]
+        private static extern uint GetPixel(IntPtr dc, int x, int y);
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern int ReleaseDC(IntPtr window, IntPtr dc);
+        [DllImport("user32.dll")]
+        private static extern bool GetCursorPos(out POINT lpPoint);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        private static Color GetColorAt(int x, int y)
+        {
+            var desk = GetDesktopWindow();
+            var dc = GetWindowDC(desk);
+            var a = (int)GetPixel(dc, x, y);
+            ReleaseDC(desk, dc);
+            return new Color(((a >> 0) & 0xff) / 255f, ((a >> 8) & 0xff) / 255f, ((a >> 16) & 0xff) / 255f);
+        }
+
+        public static Color Get()
+        {
+            GetCursorPos(out POINT cursorPos);
+            return GetColorAt(cursorPos.X, cursorPos.Y);
+        }
+    }
+}
---
+++ Shared/Shared.Info.cs
@@ -0,0 +1,49 @@
+﻿using BetterColorPicker;
+using System.Reflection;
+
+#region Assembly attributes
+
+/*
+ * These attributes define various meta-information of the generated DLL.
+ * In general, you don't need to touch these. Instead, edit the values in Info.
+ */
+[assembly: AssemblyTitle(Info.Prefix + "_" + Info.PluginName + " (" + Info.GUID + ")")]
+[assembly: AssemblyProduct(Info.Prefix + "_" + Info.PluginName)]
+[assembly: AssemblyCopyright("Copyright © - ManlyMarco 2018-2021")]
+[assembly: AssemblyVersion(Info.Version)]
+[assembly: AssemblyFileVersion(Info.Version)]
+
+#endregion Assembly attributes
+
+namespace BetterColorPicker
+{
+    /// <summary>
+    /// The main meta-data of the plug-in.
+    /// This information is used for BepInEx plug-in meta-data.
+    /// </summary>
+    public static class Info
+    {
+        /// <summary>
+        /// Human-readable name of the plug-in.
+        /// </summary>
+        public const string PluginName = "Better Color Picker";
+
+        /// <summary>
+        /// Unique ID of the plug-in.
+        /// </summary>
+        public const string GUID = "marco.better_color_picker";
+
+        /// <summary>
+        /// Version of the plug-in. Must be in form /<major/>./<minor/>./<build/>./<revision/>.
+        /// Major and minor versions are mandatory, but build and revision can be left unspecified.
+        /// </summary>
+        public const string Version = "2.0.2.0";
+
+#if KK
+        internal const string Prefix = "KK";
+#elif KKS
+        internal const string Prefix = "KKS";
+#endif
+
+    }
+}
